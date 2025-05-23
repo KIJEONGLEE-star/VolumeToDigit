86c96b3659060cb87ea1ef4762db374669b915c1
KIJEONGLEE-star <opencv45@gmail.com>
2025-05-23 14:32:10 +0900

    Create resource.h
    초기생성

---
+++ resource.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define IDD_MAIN_DIALOG     101
+#define IDC_SLIDER          1001
+#define IDC_STATIC_VALUE    1002
+#define IDC_RADIO1          1003
+#define IDC_RADIO2          1004
+#define IDC_RADIO3          1005
+#define IDC_CHECK_MUTE      1006
+#define IDC_CALC_BTN        1007
+#define IDC_OUTPUT_BOX      1008
+#define IDC_STATIC_MIN_VALUE 1009 // Compile error to protect last empty line
