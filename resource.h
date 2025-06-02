2cc03a09d6c85374a7cfdad79321b949cdccdbcb
KIJEONGLEE-star <opencv45@gmail.com>
2025-06-02 19:13:59 +0900

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,12 +1,30 @@
 #pragma once
 
-#define IDD_MAIN_DIALOG     101
-#define IDC_SLIDER          1001
-#define IDC_STATIC_VALUE    1002
-#define IDC_RADIO1          1003
-#define IDC_RADIO2          1004
-#define IDC_RADIO3          1005
-#define IDC_CHECK_MUTE      1006
-#define IDC_CALC_BTN        1007
-#define IDC_OUTPUT_BOX      1008
-#define IDC_STATIC_MIN_VALUE 1009 // Compile error to protect last empty line
+#define IDD_MAIN_DIALOG         101
+
+// 볼륨 조절 관련 컨트롤
+#define IDC_SLIDER              1001
+#define IDC_STATIC_VALUE        1002
+#define IDC_RADIO1              1003
+#define IDC_RADIO2              1004
+#define IDC_RADIO3              1005
+#define IDC_CHECK_MUTE          1006
+#define IDC_CALC_BTN            1007
+#define IDC_OUTPUT_BOX          1008
+#define IDC_STATIC_MIN_VALUE    1009
+#define IDC_COMP_CALC_BTN       1010
+
+// dB → Binary 변환 관련
+#define IDC_INPUT_DB            2001
+#define IDC_CONVERT_BTN         2002
+#define IDC_OUTPUT_BIN          2003
+
+// 보수 방식(HACOP) 계산기 관련
+#define IDC_COMP_MIN            2010
+#define IDC_COMP_MAX            2011
+#define IDC_COMP_BITS           2012
+#define IDC_COMP_OUTPUT_BOX     2014
+
+// 개발자 정보
+#define IDC_STATIC_CREDIT       2015
+#define IDC_COMP_UNIT_LABEL     2017
